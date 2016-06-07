fbd1d1052946e8d2ddf262906290f380b424401f
Hajime Tazaki <thehajime@gmail.com>
2016-06-08 07:57:15 +0900

    update tcp-rr, udp-stream bench and plot
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- lkl/netperf-bench.sh
+++ lkl/netperf-bench.sh
@@ -1,26 +1,42 @@
 #!/bin/sh
 
-TESTNAMES="TCP_STREAM TCP_RR UDP_STREAM omni"
+TESTNAMES="TCP_STREAM"
+#TESTNAMES=""
 DEST_ADDR="1.1.1.2"
 SELF_ADDR="1.1.1.5"
+export FIXED_ADDRESS=1.1.1.3
+export FIXED_MASK=24
 TRIALS=1
-LKLMUSL_NETPERF=/home/tazaki/work/netperf2/src/netperf
+
+LKLMUSL_NETPERF=/home/tazaki/work/netperf2/lkl/orig/netperf
+LKLMUSL_NETPERF_skb_pre=/home/tazaki/work/netperf2/lkl/src/netperf
+LKLMUSL_NETPERF_mmsg=/home/tazaki/work/netperf2/sendmmsg/src/netperf
 NATIVE_NETPERF=/home/tazaki/work/netperf-2.7.0/src/netperf
+NATIVE_NETPERF_mmsg=/home/tazaki/work/netperf2/native-mmsg/src/netperf
 PATH=${PATH}:/home/tazaki/work/frankenlibc/rump/bin/:/home/tazaki/work/lkl-linux/tools/lkl/bin/
 
 OUTPUT=`date -I`
 
 mkdir -p ${OUTPUT}
 
-for num in `seq 1 ${TRIALS}`
-do
-for test in ${TESTNAMES}
-do
 
-NETPERF_ARGS="-H ${DEST_ADDR} -t $test -- -o"
+run_netperf_turn()
+{
+
+test=$1
+num=$2
+ex_arg=$3
+
+NETPERF_ARGS="-H ${DEST_ADDR} -t $test -- -o $ex_arg"
 
 echo "== lkl-musl ($test-$num) =="
-rexec ${LKLMUSL_NETPERF} tap:tap0 -- ${NETPERF_ARGS} |& tee ${OUTPUT}/netperf-$test-musl-$num.dat
+rexec ${LKLMUSL_NETPERF} tap:tap0 -- ${NETPERF_ARGS} |& tee -a ${OUTPUT}/netperf-$test-musl-$num.dat
+
+echo "== lkl-musl (skb pre allocation) ($test-$num) =="
+rexec ${LKLMUSL_NETPERF_skb_pre} tap:tap0 -- ${NETPERF_ARGS} |& tee -a ${OUTPUT}/netperf-$test-musl-skbpre-$num.dat
+
+echo "== lkl-musl (sendmmsg) ($test-$num) =="
+rexec ${LKLMUSL_NETPERF_mmsg} tap:tap0 -- ${NETPERF_ARGS} |& tee -a ${OUTPUT}/netperf-$test-musl-sendmmsg-$num.dat
 
 echo "== lkl-hijack ($test-$num)  =="
 LKL_HIJACK_NET_IFTYPE=tap \
@@ -29,12 +45,45 @@ LKL_HIJACK_NET_IFTYPE=tap \
  LKL_HIJACK_NET_NETMASK_LEN=24 \
  lkl-hijack.sh \
  ${NATIVE_NETPERF} ${NETPERF_ARGS} \
- |& tee ${OUTPUT}/netperf-$test-hijack-$num.dat
+ |& tee -a ${OUTPUT}/netperf-$test-hijack-$num.dat
 
 echo "== native ($test-$num)  =="
-${NATIVE_NETPERF} ${NETPERF_ARGS} |& tee ${OUTPUT}/netperf-$test-native-$num.dat
+${NATIVE_NETPERF} ${NETPERF_ARGS} |& tee -a ${OUTPUT}/netperf-$test-native-$num.dat
+
+echo "== native (sendmmsg) ($test-$num)  =="
+${NATIVE_NETPERF_mmsg} ${NETPERF_ARGS} |& tee -a ${OUTPUT}/netperf-$test-native-mmsg-$num.dat
+
+
+}
+
+
+rm -f ${OUTPUT}/netperf-*.dat
+
+# for TCP_xx tests
+for num in `seq 1 ${TRIALS}`
+do
+for test in ${TESTNAMES}
+do
+
+run_netperf_turn $test $num
 
 done
 done
+
+# UDP_STREAM with different packet size
+
+for num in `seq 1 ${TRIALS}`
+do
+for size in 64 128 256 512 1024 1500 2048 65507
+do
+
+run_netperf_turn TCP_RR $num " -r $size,$size"
+
+run_netperf_turn UDP_STREAM $num "LOCAL_SEND_SIZE,THROUGHPUT,THROUGHPUT_UNITS,REMOTE_RECV_CALLS,ELAPSED_TIME -m $size"
+
+done
+done
+
+
 
 sh `dirname ${BASH_SOURCE:-$0}`/netperf-plot.sh ${OUTPUT}
--- lkl/netperf-plot.sh
+++ lkl/netperf-plot.sh
@@ -1,26 +1,32 @@
 
 OUTPUT=$1
+PKG_SIZES="64 128 256 512 1024 1500 2048"
+
 
 # parse outputs
 
 grep -h bits ${OUTPUT}/netperf-TCP_ST*-hijack* > ${OUTPUT}/tcp-stream-hijack.dat
-grep -h bits ${OUTPUT}/netperf-TCP_ST*-musl* > ${OUTPUT}/tcp-stream-musl.dat
+grep -E -h bits ${OUTPUT}/netperf-TCP_ST*-musl-[0-9].* > ${OUTPUT}/tcp-stream-musl.dat
+grep -h bits ${OUTPUT}/netperf-TCP_ST*-musl-skbpre* > ${OUTPUT}/tcp-stream-musl-skbpre.dat
+grep -h bits ${OUTPUT}/netperf-TCP_ST*-musl-sendmmsg* > ${OUTPUT}/tcp-stream-musl-sendmmsg.dat
 grep -h bits ${OUTPUT}/netperf-TCP_ST*-native* > ${OUTPUT}/tcp-stream-native.dat
 
+
 grep -h Trans ${OUTPUT}/netperf-TCP_RR*-hijack* > ${OUTPUT}/tcp-rr-hijack.dat
-grep -h Trans ${OUTPUT}/netperf-TCP_RR*-musl* > ${OUTPUT}/tcp-rr-musl.dat
+grep -E -h Trans ${OUTPUT}/netperf-TCP_RR*-musl-[0-9].* > ${OUTPUT}/tcp-rr-musl.dat
+grep -h Trans ${OUTPUT}/netperf-TCP_RR*-musl-skbpre* > ${OUTPUT}/tcp-rr-musl-skbpre.dat
+grep -h Trans ${OUTPUT}/netperf-TCP_RR*-musl-sendmmsg* > ${OUTPUT}/tcp-rr-musl-sendmmsg.dat
 grep -h Trans ${OUTPUT}/netperf-TCP_RR*-native* > ${OUTPUT}/tcp-rr-native.dat
 
-grep -h bits ${OUTPUT}/netperf-UDP_ST*-hijack* > ${OUTPUT}/udp-stream-hijack.dat
-grep -h bits ${OUTPUT}/netperf-UDP_ST*-musl* > ${OUTPUT}/udp-stream-musl.dat
-grep -h bits ${OUTPUT}/netperf-UDP_ST*-native* > ${OUTPUT}/udp-stream-native.dat
 
-grep -h bits ${OUTPUT}/netperf-omni*-hijack* > ${OUTPUT}/omni-hijack.dat
-grep -h bits ${OUTPUT}/netperf-omni*-musl* > ${OUTPUT}/omni-musl.dat
-grep -h bits ${OUTPUT}/netperf-omni*-native* > ${OUTPUT}/omni-native.dat
+grep -h bits ${OUTPUT}/netperf-UDP_ST*-hijack* > ${OUTPUT}/udp-stream-hijack.dat
+grep -E -h bits ${OUTPUT}/netperf-UDP_ST*-musl-[0-9].* > ${OUTPUT}/udp-stream-musl.dat
+grep -h bits ${OUTPUT}/netperf-UDP_ST*-musl-skbpre* > ${OUTPUT}/udp-stream-musl-skbpre.dat
+grep -h bits ${OUTPUT}/netperf-UDP_ST*-musl-sendmmsg* > ${OUTPUT}/udp-stream-musl-sendmmsg.dat
+grep -E -h bits ${OUTPUT}/netperf-UDP_ST*-native-[0-9]* > ${OUTPUT}/udp-stream-native.dat
+grep -h bits ${OUTPUT}/netperf-UDP_ST*-native-mmsg* > ${OUTPUT}/udp-stream-native-sendmmsg.dat
 
 gnuplot  << EndGNUPLOT
-set ylabel "Goodput (Mbps)"
 set terminal postscript eps lw 3 "Helvetica" 24
 set output "${OUTPUT}/tcp-stream.eps"
 set yrange [0:]
@@ -30,34 +36,53 @@ set xzeroaxis
 set grid ytics
 
 set key left top
-set boxwidth 0.1
+set boxwidth 0.2
 set style fill pattern
 
 set datafile separator "," 
 
+set ylabel "Goodput (Mbps)"
 plot \
-   '${OUTPUT}/tcp-stream-hijack.dat' usin (\$0-0.1):5 w boxes  title "hijack", \
-   '${OUTPUT}/tcp-stream-musl.dat' usin 0:5 w boxes  title "lkl-musl", \
-   '${OUTPUT}/tcp-stream-native.dat' usin (\$0+0.1):5 w boxes  title "native"
+   '${OUTPUT}/tcp-stream-hijack.dat' usin (\$0-0.4):5 w boxes  title "hijack", \
+   '${OUTPUT}/tcp-stream-musl.dat' usin (\$0-0.2):5 w boxes  title "lkl-musl", \
+   '${OUTPUT}/tcp-stream-musl-skbpre.dat' usin (\$0+0.0):5 w boxes  title "lkl-musl (skb prealloc)", \
+   '${OUTPUT}/tcp-stream-musl-sendmmsg.dat' usin (\$0+0.2):5 w boxes  title "lkl-musl (sendmmsg)", \
+   '${OUTPUT}/tcp-stream-native.dat' usin (\$0+0.4):5 w boxes  title "native"
 
-set output "${OUTPUT}/omni.eps"
-plot \
-   '${OUTPUT}/omni-hijack.dat' usin (\$0-0.1):5 w boxes  title "hijack", \
-   '${OUTPUT}/omni-musl.dat' usin 0:5 w boxes  title "lkl-musl", \
-   '${OUTPUT}/omni-native.dat' usin (\$0+0.1):5 w boxes  title "native"
-
-set output "${OUTPUT}/udp-stream.eps"
-plot \
-   '${OUTPUT}/udp-stream-hijack.dat' usin (\$0-0.1):5 w boxes  title "hijack", \
-   '${OUTPUT}/udp-stream-musl.dat' usin 0:5 w boxes  title "lkl-musl", \
-   '${OUTPUT}/udp-stream-native.dat' usin (\$0+0.1):5 w boxes  title "native"
-
+set xtics ("64" 0, "128" 1, "256" 2, "512" 3, "1024" 4, "1500" 5, "2048" 6, "65507" 7)
+set xlabel "Payload size (bytes)"
+set xrange [-1:8]
 set output "${OUTPUT}/tcp-rr.eps"
 set ylabel "Goodput (Trans/sec)"
+
 plot \
-   '${OUTPUT}/tcp-rr-hijack.dat' usin (\$0-0.1):8 w boxes  title "hijack", \
-   '${OUTPUT}/tcp-rr-musl.dat' usin 0:8 w boxes  title "lkl-musl", \
-   '${OUTPUT}/tcp-rr-native.dat' usin (\$0+0.1):8 w boxes  title "native"
+   '${OUTPUT}/tcp-rr-hijack.dat' usin (\$0-0.3):8 w boxes  title "hijack", \
+   '${OUTPUT}/tcp-rr-musl.dat' usin (\$0-0.1):8 w boxes  title "lkl-musl", \
+   '${OUTPUT}/tcp-rr-musl-skbpre.dat' usin (\$0+0.1):8 w boxes  title "lkl-musl (skb prealloc)", \
+   '${OUTPUT}/tcp-rr-native.dat' usin (\$0+0.3):8 w boxes  title "native"
+
+set output "${OUTPUT}/udp-stream.eps"
+set ylabel "Goodput (Mbps)"
+
+plot \
+   '${OUTPUT}/udp-stream-hijack.dat' usin (\$0-0.3):2 w boxes  title "hijack", \
+   '${OUTPUT}/udp-stream-musl.dat' usin (\$0-0.1):2 w boxes  title "lkl-musl", \
+   '${OUTPUT}/udp-stream-musl-sendmmsg.dat' usin (\$0+0.1):2 w boxes  title "lkl-musl (sendmmsg+skb prealloc)", \
+   '${OUTPUT}/udp-stream-native-sendmmsg.dat' usin (\$0+0.3):2 w boxes  title "native (sendmmsg)"
+
+   #'${OUTPUT}/udp-stream-musl-skbpre.dat' usin (\$0-0.1):2 w boxes  title "lkl-musl (skb prealloc)", \
+   #'${OUTPUT}/udp-stream-native.dat' usin (\$0+0.4):2 w boxes  title "native"
+
+
+set output "${OUTPUT}/udp-stream-pps.eps"
+set ylabel "Throughput (pps)"
+
+plot \
+   '${OUTPUT}/udp-stream-hijack.dat' usin (\$0-0.3):(\$4/\$5) w boxes  title "hijack", \
+   '${OUTPUT}/udp-stream-musl.dat' usin (\$0-0.1):(\$4/\$5) w boxes  title "lkl-musl", \
+   '${OUTPUT}/udp-stream-musl-sendmmsg.dat' usin (\$0+0.1):(\$4/\$5) w boxes  title "lkl-musl (sendmmsg+skb prealloc)", \
+   '${OUTPUT}/udp-stream-native-sendmmsg.dat' usin (\$0+0.3):(\$4/\$5) w boxes  title "native (sendmmsg)"
+
 
 # set terminal png lw 3 16
 # set xtics nomirror rotate by -45 font ",16"
