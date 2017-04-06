618a863a57d7afb0ca28d5c2943bf01eaf0be90d
midchildan <midchildan+git@gmail.com>
2017-04-06 18:53:52 +0900

    Add netbsd version of netperf

--- lkl/netperf-bench.sh
+++ lkl/netperf-bench.sh
@@ -1,99 +1,97 @@
-#!/bin/sh
+#!/bin/bash
 
-source ./netperf-common.sh
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+source "$SCRIPT_DIR/netperf-common.sh"
 
 PREFIX=netperf-bench
 TESTNAMES="TCP_STREAM TCP_MAERTS"
 DEST_ADDR="1.1.1.2"
 SELF_ADDR="1.1.1.3"
-export FIXED_ADDRESS=${SELF_ADDR}
-export FIXED_MASK=24
 OIF=ens3f1
 OIF=br0
 
-mkdir -p ${OUTPUT}
+initialize() {
+  export FIXED_ADDRESS=${SELF_ADDR}
+  export FIXED_MASK=24
 
-export LKL_HIJACK_SYSCTL="net.ipv4.tcp_wmem=4096 87380 100000000"
-export LKL_HIJACK_BOOT_CMDLINE="mem=1G"
+  export LKL_HIJACK_SYSCTL="net.ipv4.tcp_wmem=4096 87380 100000000"
+  export LKL_HIJACK_BOOT_CMDLINE="mem=1G"
 
-export LKL_SYSCTL="net.ipv4.tcp_wmem=4096 87380 100000000"
-export LKL_BOOT_CMDLINE="mem=1G"
+  export LKL_SYSCTL="net.ipv4.tcp_wmem=4096 87380 100000000"
+  export LKL_BOOT_CMDLINE="mem=1G"
 
-# enable offload
+  # enable offload
 
-# disable c-state
-sudo tuned-adm profile latency-performance
+  # disable c-state
+  sudo tuned-adm profile latency-performance
 
-# VIRTIO offloads, CSUM/TSO4/MRGRCVBUF/UFO
-export LKL_HIJACK_OFFLOAD=0xc803
-
-
-exec > >(tee ${OUTPUT}/$0.log) 2>&1
-
-run_netperf_turn()
-{
-
-test=$1
-num=$2
-psize=$3
-ex_arg=$4
+  # VIRTIO offloads, CSUM/TSO4/MRGRCVBUF/UFO
+  export LKL_HIJACK_OFFLOAD=0xc803
 
 
-NETPERF_ARGS="-H ${DEST_ADDR} -t $test -- -o $ex_arg"
-
-sudo ethtool -K ${OIF} tso on gro on gso on rx on tx on
-sudo sysctl -w net.ipv4.tcp_wmem="4096 87380 100000000"
-
-# XXX: musl-libc w/ UDP_STREAM is not working over 2sec length so, use hijack
-if [ $test == "UDP_STREAM" ] ; then
-echo "== lkl-hijack tap ($test-$num $*)  =="
-LKL_HIJACK_NET_IFTYPE=tap \
- LKL_HIJACK_NET_IFPARAMS=tap0 \
- LKL_HIJACK_NET_IP=${SELF_ADDR} \
- LKL_HIJACK_NET_NETMASK_LEN=24 \
-${TASKSET} lkl-hijack.sh \
- ${NATIVE_NETPERF}/netperf ${NETPERF_ARGS} \
- |& tee -a ${OUTPUT}/${PREFIX}-$test-hijack-tap-ps$size-$num.dat
-
-else
-echo "== lkl-musl tap ($test-$num $*)  =="
-
-rexec ${LKLMUSL_NETPERF}/netperf tap:tap0 -- ${NETPERF_ARGS} \
- |& tee -a ${OUTPUT}/${PREFIX}-$test-musl-tap-ps$size-$num.dat
-fi
-
-echo "== native ($test-$num $*)  =="
-NETPERF_ARGS="-H ${DEST_ADDR} -t $test -- -o $ex_arg"
-${TASKSET} ${NATIVE_NETPERF}/netperf ${NETPERF_ARGS} |& tee -a ${OUTPUT}/${PREFIX}-$test-native-ps$size-$num.dat
-
-#echo "== native (sendmmsg) ($test-$num)  =="
-#${TASKSET} ${NATIVE_NETPERF_mmsg}/netperf ${NETPERF_ARGS} |& tee -a ${OUTPUT}/netperf-$test-native-mmsg-$num.dat
-#
-
+  mkdir -p "$OUTPUT"
+  rm -f ${OUTPUT}/${PREFIX}-*.dat
+  exec > >(tee ${OUTPUT}/$0.log) 2>&1
 }
 
-
-rm -f ${OUTPUT}/${PREFIX}-*.dat
-
-
-# for netperf tests
-for num in `seq 1 ${TRIALS}`
-do
-for size in ${PSIZES}
-do
-
-for test in ${TESTNAMES}
-do
-
-run_netperf_turn $test $num $size "-m $size,$size"
-
-done
-
-run_netperf_turn TCP_RR $num $size " -r $size,$size"
-run_netperf_turn UDP_STREAM $num $size "LOCAL_SEND_SIZE,THROUGHPUT,THROUGHPUT_UNITS,REMOTE_RECV_CALLS,ELAPSED_TIME -m $size"
-done
-done
+run_netperf_turn() {
+  local test="$1"
+  local num="$2"
+  local psize="$3"
+  local ex_arg="$4"
 
 
-sh `dirname ${BASH_SOURCE:-$0}`/netperf-plot.sh ${OUTPUT} tx
-sh `dirname ${BASH_SOURCE:-$0}`/netperf-plot.sh ${OUTPUT} rx
+  local NETPERF_ARGS="-H $DEST_ADDR -t $test -- -o $ex_arg"
+
+  sudo ethtool -K ${OIF} tso on gro on gso on rx on tx on
+  sudo sysctl -w net.ipv4.tcp_wmem="4096 87380 100000000"
+
+  # XXX: musl-libc w/ UDP_STREAM is not working over 2sec length so, use hijack
+  if [[ "$test" == "UDP_STREAM" ]] ; then
+    echo "$(tput bold)== lkl-hijack tap ($test-$num $*)  ==$(tput srg0)"
+    LKL_HIJACK_NET_IFTYPE=tap \
+    LKL_HIJACK_NET_IFPARAMS=tap0 \
+    LKL_HIJACK_NET_IP=${SELF_ADDR} \
+    LKL_HIJACK_NET_NETMASK_LEN=24 \
+      $TASKSET lkl-hijack.sh \
+      "$NATIVE_NETPERF/netperf" $NETPERF_ARGS \
+        |& tee -a "$OUTPUT/$PREFIX-$test-hijack-tap-ps$size-$num.dat"
+  else
+    echo "$(tput bold)== lkl-musl tap ($test-$num $*)  ==$(tput sgr0)"
+
+    rexec "$LKLMUSL_NETPERF/netperf" tap:tap0 -- $NETPERF_ARGS \
+     |& tee -a "$OUTPUT/$PREFIX-$test-musl-tap-ps$size-$num.dat"
+  fi
+
+  echo "$(tput bold)== netbsd tap ($test-$num $*)  ==$(tput sgr0)"
+  rexec "$NETBSD_NETPERF/netperf" tap:tap0 -- $NETPERF_ARGS \
+    |& tee -a "$OUTPUT/$PREFIX-$test-netbsd-tap-ps$size-$num.dat"
+
+  echo "$(tput bold)== native ($test-$num $*)  ==$(tput sgr0)"
+  NETPERF_ARGS="-H $DEST_ADDR -t $test -- -o $ex_arg"
+  $TASKSET "$NATIVE_NETPERF/netperf" $NETPERF_ARGS |& tee -a "$OUTPUT/$PREFIX-$test-native-ps$size-$num.dat"
+
+  #echo "== native (sendmmsg) ($test-$num)  =="
+  #$TASKSET "$NATIVE_NETPERF_mmsg/netperf" $NETPERF_ARGS |& tee -a "$OUTPUT/netperf-$test-native-mmsg-$num.dat"
+}
+
+main() {
+  initialize
+
+  # for netperf tests
+  for num in $(seq 1 "$TRIALS"); do
+    for size in $PSIZES; do
+      for test in $TESTNAMES; do
+        run_netperf_turn $test $num $size "-m $size,$size"
+      done
+
+      run_netperf_turn TCP_RR $num $size " -r $size,$size"
+      run_netperf_turn UDP_STREAM $num $size "LOCAL_SEND_SIZE,THROUGHPUT,THROUGHPUT_UNITS,REMOTE_RECV_CALLS,ELAPSED_TIME -m $size"
+    done
+  done
+
+  bash "$SCRIPT_DIR/netperf-plot.sh" "$OUTPUT" tx
+  bash "$SCRIPT_DIR/netperf-plot.sh" "$OUTPUT" rx
+}
+
+main
--- lkl/netperf-common.sh
+++ lkl/netperf-common.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 PSIZES="64 128 256 512 1024 1500 65507"
 PSIZE_XTICS="('64' 0, '128' 1, '256' 2, '512' 3, '1024' 4, '1500' 5, '65507' 6)"
 SYS_MEM="512M 2G"
@@ -9,13 +11,15 @@ OFFLOADS="0 0003 d903" #disable, CSUM, CSUM/TSO4/MRGRCVBUF/UFO + TSO6
 
 TRIALS=5
 
-LKL_DIR=/home/tazaki/work/lkl-linux/
-LKLMUSL_NETPERF=/home/tazaki/work/netperf2/lkl/src/
-NATIVE_NETPERF=/home/tazaki/work/netperf-2.7.0/src/
-PATH=${PATH}:/home/tazaki/work/frankenlibc/rump/bin/:${LKL_DIR}/tools/lkl/bin
+[[ -z "$PROJECT_ROOT" ]] && PROJECT_ROOT=~/work
+LKL_DIR="$PROJECT_ROOT/lkl-linux/"
+PATH+=":$PROJECT_ROOT/frankenlibc/rump/bin/:$LKL_DIR/tools/lkl/bin"
+NATIVE_NETPERF="$PROJECT_ROOT/netperf2/src/"
+LKLMUSL_NETPERF="$PROJECT_ROOT/netperf2-lklmusl/src/"
+NETBSD_NETPERF="$PROJECT_ROOT/netperf2-netbsd/src/"
 
 TASKSET="taskset -c 0"
-OUTPUT=`date -I`
+OUTPUT="$(date -I)"
 
 export LKL_HIJACK_OFFLOAD=0xc803
 export LKL_OFFLOAD=1
@@ -23,4 +27,3 @@ export LKL_OFFLOAD=1
 #export LKL_HIJACK_BOOT_CMDLINE="mem=1G"
 #LKL_SYSCTL="net.ipv4.tcp_wmem=4096 87380 2147483647"
 #LKL_BOOT_CMDLINE="mem=1G"
-
