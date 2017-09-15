99466289ad12b747d7af7f3b5a5f9320857f6225
Hajime Tazaki <thehajime@gmail.com>
2017-09-15 09:14:30 +0900

    add netperf benchmark scripts on android
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

---
+++ lkl/netperf-bench-android.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+source "$SCRIPT_DIR/netperf-common.sh"
+
+PREFIX=netperf-bench
+TESTNAMES="TCP_STREAM TCP_MAERTS"
+DEST_ADDR="1.1.1.2"
+SELF_ADDR="1.1.1.3"
+OIF=ens3f1
+OIF=br0
+
+main() {
+  initialize
+
+  # for netperf tests
+  for num in $(seq 1 "$TRIALS"); do
+    for size in $PSIZES; do
+      for test in $TESTNAMES; do
+        netperf::run $test $num $size "-m $size,$size"
+      done
+
+#      nerperf::run TCP_RR $num $size " -r $size,$size"
+#      nerperf::run UDP_STREAM $num $size "LOCAL_SEND_SIZE,THROUGHPUT,THROUGHPUT_UNITS,REMOTE_RECV_CALLS,ELAPSED_TIME -m $size"
+    done
+  done
+
+  bash "$SCRIPT_DIR/netperf-plot-android.sh" "$OUTPUT" tx
+  bash "$SCRIPT_DIR/netperf-plot-android.sh" "$OUTPUT" rx
+}
+
+initialize() {
+  export FIXED_ADDRESS=${SELF_ADDR}
+  export FIXED_MASK=24
+
+  export LKL_HIJACK_SYSCTL="net.ipv4.tcp_wmem=4096 87380 100000000"
+  export LKL_HIJACK_BOOT_CMDLINE="mem=1G"
+
+  export LKL_SYSCTL="net.ipv4.tcp_wmem=4096 87380 100000000"
+  export LKL_BOOT_CMDLINE="mem=1G"
+
+  # enable offload
+
+  # disable c-state
+  sudo tuned-adm profile latency-performance
+
+  # VIRTIO offloads, CSUM/TSO4/MRGRCVBUF/UFO
+  export LKL_HIJACK_OFFLOAD=0xc803
+
+  mkdir -p "$OUTPUT"
+  rm -f "$OUTPUT/$PREFIX"-*.dat
+  exec > >(tee "$OUTPUT/$0.log") 2>&1
+}
+
+netperf::run() {
+  sudo ethtool -K "$OIF" tso on gro on gso on rx on tx on
+  sudo sysctl -w net.ipv4.tcp_wmem="4096 87380 100000000"
+
+  netperf::lkl_android "$@"
+  netperf::native_android "$@"
+}
+
+
+netperf::lkl_android() {
+(
+  local test="$1"
+  local num="$2"
+  local psize="$3"
+  local ex_arg="$4"
+  local netperf_args="-H 202.214.86.51 -c -C -D1 -T 1/1 -t $test -p 443 -- -o $ex_arg -P 80,80"
+
+  setup_lkl
+
+  export PATH=${PATH}:/home/tazaki/Android/Sdk/platform-tools/
+  echo "$(tput bold)== lkl-android raw ($test-$num $*)  ==$(tput sgr0)"
+
+  #adb shell su -c /data/local/tmp/cellar-test.sh /data/local/tmp/netperf $netperf_args \
+
+  adb shell su -c /data/local/tmp/mptcp.sh /data/local/tmp/netperf $netperf_args \
+     |& tee -a "$OUTPUT/$PREFIX-$test-lkl-android-raw-ps$size-$num.dat"
+)
+}
+
+netperf::native_android() {
+(
+  local test="$1"
+  local num="$2"
+  local psize="$3"
+  local ex_arg="$4"
+  local netperf_args="-H 202.214.86.51 -c -C -D1 -T 1/1 -t $test -p 443 -- -o $ex_arg -P 80,80"
+
+  setup_lkl
+
+  export PATH=${PATH}:/home/tazaki/Android/Sdk/platform-tools/
+  echo "$(tput bold)== native-android ($test-$num $*)  ==$(tput sgr0)"
+
+  adb shell /data/local/tmp/netperf $netperf_args \
+     |& tee -a "$OUTPUT/$PREFIX-$test-native-android-raw-ps$size-$num.dat"
+)
+}
+
+main
---
+++ lkl/netperf-plot-android.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+source "$SCRIPT_DIR/netperf-common.sh"
+
+OUTPUT="$1"
+DIR=tx
+PREFIX=netperf-bench-TCP_STREAM
+PREFIX_RR=netperf-bench-TCP_RR
+PREFIX_UDP=netperf-bench-UDP_STREAM
+
+if [[ "$2" == "rx" ]] ; then
+  DIR=rx
+  PREFIX=netperf-bench-TCP_MAERTS
+elif [[ "$2" == "tx" ]] ; then
+  DIR=tx
+  PREFIX=netperf-bench-TCP_STREAM
+else
+  echo "Invalid direction: $0 \${OUTPUT} [tx/rx]"
+  exit
+fi
+
+rm -rf "$OUTPUT/$DIR"
+mkdir -p "$OUTPUT/$DIR"
+mkdir -p "$OUTPUT/out/$DIR/"
+
+
+# parse outputs
+
+# TCP_STREAM
+
+grep -E -h bits ${OUTPUT}/${PREFIX}*-native-android-* | grep usec \
+| dbcoldefine dum | csv_to_db | dbcoldefine  d1 d2 psize d3 thpt d4 \
+| dbmultistats -k psize thpt | dbsort -n psize | dbcol mean stddev \
+> ${OUTPUT}/${DIR}/tcp-stream-native-android.dat
+
+grep -E -h bits ${OUTPUT}/${PREFIX}*-lkl-android-* | grep usec \
+| dbcoldefine dum | csv_to_db | dbcoldefine  d1 d2 psize d3 thpt d4 \
+| dbmultistats -k psize thpt | dbsort -n psize | dbcol mean stddev \
+> ${OUTPUT}/${DIR}/tcp-stream-lkl-android.dat
+
+# cpu utilization
+grep -E -h bits ${OUTPUT}/${PREFIX}*-native-android-* | grep usec \
+| dbcoldefine dum | csv_to_db | dbcoldefine  d1 d2 psize d3 d4 d5 cpu \
+| dbmultistats -k psize cpu | dbsort -n psize | dbcol mean stddev \
+> ${OUTPUT}/${DIR}/tcp-stream-native-android-cpu.dat
+
+grep -E -h bits ${OUTPUT}/${PREFIX}*-lkl-android-* | grep usec \
+| dbcoldefine dum | csv_to_db | dbcoldefine  d1 d2 psize d3 d4 d5 cpu \
+| dbmultistats -k psize cpu | dbsort -n psize | dbcol mean stddev \
+> ${OUTPUT}/${DIR}/tcp-stream-lkl-android-cpu.dat
+
+
+gnuplot  << EndGNUPLOT
+set terminal postscript eps lw 3 "Helvetica" 24
+set output "${OUTPUT}/out/${DIR}/tcp-stream.eps"
+#set xtics font "Helvetica,14"
+set pointsize 2
+set xzeroaxis
+set grid ytics
+
+set boxwidth 0.4
+set style fill pattern
+
+set size 1.0,0.6
+set key top left
+
+set xrange [-1:7]
+set xtics ${PSIZE_XTICS}
+set xlabel "Payload size (bytes)"
+set yrange [0:100]
+set ylabel "Goodput (Mbps)"
+set y2tics
+set y2range [0:50]
+set y2label "CPU utilization (%)"
+
+
+plot \
+   '${OUTPUT}/${DIR}/tcp-stream-lkl-android.dat' usin (\$0-0.2):(\$1):(\$2) w boxerrorbar fill patter 0 title "LKL" , \
+   '${OUTPUT}/${DIR}/tcp-stream-native-android.dat' usin (\$0+0.2):(\$1):(\$2) w boxerrorbar fill patter 0 title "native" ,\
+   '${OUTPUT}/${DIR}/tcp-stream-lkl-android-cpu.dat' usin (\$0-0.2):(\$1):(\$2) w yerrorbars axes x1y2 notitle , \
+   '${OUTPUT}/${DIR}/tcp-stream-native-android.dat' usin (\$0+0.2):(\$1):(\$2) w yerrorbars axes x1y2 notitle 
+
+set terminal png lw 3 14 crop
+set output "${OUTPUT}/out/${DIR}/tcp-stream.png"
+replot
+
+
+set terminal dumb
+unset output
+replot
+
+quit
+EndGNUPLOT
+
