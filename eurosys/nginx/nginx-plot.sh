78d7aea1d1c7c2278b2802811eac821841033dca
Hajime Tazaki <thehajime@gmail.com>
2018-09-18 12:08:00 +0900

    split figure

--- eurosys/nginx/nginx-plot.sh
+++ eurosys/nginx/nginx-plot.sh
@@ -37,6 +37,47 @@ set style fill pattern
 set key top righ
 set size 1.0,0.7
 
+# trans/sec
+set ylabel "Throughput (KReq/sec)"
+set ytics 5
+set yrange [0:20]
+set xtics ("64" 0, "128" 1, "256" 2, "512" 3, "1024" 4, "1500" 5, "2048" 6)
+set xlabel "File size (bytes)"
+set xrange [-1:7]
+set terminal postscript eps lw 3 "Helvetica" 24
+set output "${OUTPUT}/out/nginx-wrk-thpt.eps"
+
+plot \
+   '${OUTPUT}/nginx-docker-thpt.dat' usi (\$0-0.3):(\$1/1000):(\$2/1000) w boxerr lc rgb "green" fill pattern 2 title "docker(mac)" ,\
+   '${OUTPUT}/nginx-lkl-thpt.dat' usi (\$0-0):(\$1/1000):(\$2/1000) w boxerr lc rgb "cyan" fill pattern 4 title "lkl", \
+   '${OUTPUT}/nginx-native-thpt.dat' usi (\$0+0.3):(\$1/1000):(\$2/1000) w boxerr lc rgb "red" fill pattern 0 title "native(mac)" 
+
+set terminal png lw 3 14 crop
+set xtics nomirror font ",14"
+set output "${OUTPUT}/out/nginx-wrk-thpt.png"
+replot
+
+# latency
+set ylabel "Latency (msec)"
+set ytics 1
+set yrange [0:5]
+set xtics ("64" 0, "128" 1, "256" 2, "512" 3, "1024" 4, "1500" 5, "2048" 6)
+set xlabel "File size (bytes)"
+set xrange [-1:7]
+set terminal postscript eps lw 3 "Helvetica" 24
+set output "${OUTPUT}/out/nginx-wrk-latency.eps"
+
+plot \
+   '${OUTPUT}/nginx-docker.dat' usin (\$0-0.3):(\$1/1000):(\$2/1000) w boxerr ps 1 lc rgb "green"  title "docker(mac)" ,\
+   '${OUTPUT}/nginx-lkl.dat' usin (\$0-0):(\$1/1000):(\$2/1000) w boxerr ps 1 lc rgb "cyan" title "lkl", \
+   '${OUTPUT}/nginx-native.dat' usin (\$0+0.3):(\$1/1000):(\$2/1000) w boxerr ps 1 lc rgb "red"  title "native(mac)" 
+
+set terminal png lw 3 14 crop
+set xtics nomirror font ",14"
+set output "${OUTPUT}/out/nginx-wrk-latency.png"
+replot
+
+# combined
 set ylabel "Throughput (KReq/sec)"
 set ytics 5
 set yrange [0:20]
@@ -47,7 +88,7 @@ set xtics ("64" 0, "128" 1, "256" 2, "512" 3, "1024" 4, "1500" 5, "2048" 6)
 set xlabel "File size (bytes)"
 set xrange [-1:7]
 set terminal postscript eps lw 3 "Helvetica" 24
-set output "${OUTPUT}/out/nginx-wrk.eps"
+set output "${OUTPUT}/out/nginx-wrk-combined.eps"
 
 plot \
    '${OUTPUT}/nginx-docker-thpt.dat' usi (\$0-0.3):(\$1/1000):(\$2/1000) w boxerr lc rgb "green" fill pattern 2 title "docker(mac)" ,\
@@ -59,7 +100,7 @@ plot \
 
 set terminal png lw 3 14 crop
 set xtics nomirror font ",14"
-set output "${OUTPUT}/out/nginx-wrk.png"
+set output "${OUTPUT}/out/nginx-wrk-combined.png"
 replot
 
 
