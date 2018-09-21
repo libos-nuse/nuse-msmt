f0080faf06d6c27390730522e78fc71c8d6e55a4
Hajime Tazaki <thehajime@gmail.com>
2018-09-21 13:06:29 +0900

    cosmetic

--- eurosys/py-coldstart/py-plot.sh
+++ eurosys/py-coldstart/py-plot.sh
@@ -32,17 +32,17 @@ set ylabel "Delay (msec)"
 set ytics 10
 set yrange [10:1500]
 set logscale y
-set xtics font ", 16"
+set xtics font ", 18"
 set xtics ('runc' 0, 'runv' 1, 'runsc(p)' 2, 'runsc(k)' 3, 'runu' 4, 'native' 5)
 set xrange [-1:6]
 
 plot \
-   '${OUTPUT}/py-coldstart-runc-delay.dat' usi (0):(\$1)*1000:(\$2)*1000 w boxerr lc rgb "green" fill pattern 2 title "runc", \
-   '${OUTPUT}/py-coldstart-kata-runtime-delay.dat' usi (1):(\$1)*1000:(\$2)*1000 w boxerr lc rgb "gray" fill pattern 1 title "runv", \
-   '${OUTPUT}/py-coldstart-runsc-ptrace-user-delay.dat' usi (2):(\$1)*1000:(\$2*1000) w boxerr lc rgb "blue" fill pattern 3 title "runsc(ptrace)", \
-   '${OUTPUT}/py-coldstart-runsc-kvm-user-delay.dat' usi (3):(\$1)*1000:(\$2)*1000 w boxerr lc rgb "blue" fill pattern 5 title "runsc(kvm)", \
-   '${OUTPUT}/py-coldstart-runu-delay.dat' usi (4):(\$1)*1000:(\$2)*1000 w boxerr lc rgb "cyan" fill pattern 4 title "runu", \
-   '${OUTPUT}/py-coldstart-native-delay.dat' usi (5):(\$1)*1000:(\$2)*1000 w boxerr fill patter 0 lc rgb "red" fill pattern 0 title "native"
+   '${OUTPUT}/py-coldstart-runc-delay.dat' usi (0):(\$1)*1000:(\$2)*1000 w boxerr lt 1 lc rgb "green" fill pattern 2 title "runc", \
+   '${OUTPUT}/py-coldstart-kata-runtime-delay.dat' usi (1):(\$1)*1000:(\$2)*1000 w boxerr lt 1 lc rgb "gray" fill pattern 1 title "runv", \
+   '${OUTPUT}/py-coldstart-runsc-ptrace-user-delay.dat' usi (2):(\$1)*1000:(\$2*1000) w boxerr lt 1 lc rgb "blue" fill pattern 3 title "runsc(ptrace)", \
+   '${OUTPUT}/py-coldstart-runsc-kvm-user-delay.dat' usi (3):(\$1)*1000:(\$2)*1000 w boxerr lt 1 lc rgb "blue" fill pattern 5 title "runsc(kvm)", \
+   '${OUTPUT}/py-coldstart-runu-delay.dat' usi (4):(\$1)*1000:(\$2)*1000 w boxerr lt 1 lc rgb "cyan" fill pattern 4 title "runu", \
+   '${OUTPUT}/py-coldstart-native-delay.dat' usi (5):(\$1)*1000:(\$2)*1000 w boxerr fill patter 0 lt 1 lc rgb "red" title "native"
    
 set terminal png lw 3 14 crop
 set output "${OUTPUT}/out/py-coldstart.png"
