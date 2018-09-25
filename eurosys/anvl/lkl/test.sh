fc8a708d0d16332a31a53bb4db748ec19b7c5e41
Hajime Tazaki <thehajime@gmail.com>
2018-09-25 12:28:24 +0900

    clean up test script
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- eurosys/anvl/lkl/test.sh
+++ eurosys/anvl/lkl/test.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+OUTPUT="$(date "+%Y-%m-%d")"
+
 # available TESTS="icmpv6 ipv6 ipv6-autoconfig ipv6-ndp ipv6-pmtu ipv6-rtralert ipv6cp ipv6ov4"
 TESTS="ipv6 icmpv6"
 
+mkdir -p $OUTPUT
+
+#exec > >(tee "$OUTPUT/$(basename $0).log") 2>&1
 
 for test in $TESTS
 do
-  sudo /opt/Ixia/IxANVL900/ANVL-BIN/anvl -file $test-out.log -f anvl$test $test
+  sudo /opt/Ixia/IxANVL900/ANVL-BIN/anvl -file $OUTPUT/$test-out.log \
+   -f anvl$test $test
 done
