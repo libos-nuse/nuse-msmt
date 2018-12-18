5d2af415af9cdaa8b93370cb728251d1cb8a2a58
Hajime Tazaki <thehajime@gmail.com>
2018-12-18 11:49:39 +0900

    clean plot shape and distance between bars
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- eurosys/sqlite/sqlite-plot.sh
+++ eurosys/sqlite/sqlite-plot.sh
@@ -22,13 +22,13 @@ set pointsize 2
 set xzeroaxis
 set grid ytics
 
-set boxwidth 0.2
+set boxwidth 0.15
 set style fill pattern
 
 set size 1.0,0.8
 set key top left
 
-set xrange [-1.0:7.0]
+set xrange [-0.5:6.5]
 set xtics ('128' 0, '256' 1, '512' 2, '1024' 3, '1500' 4, '4096' 5, '8192' 6)
 set xlabel "Value size (bytes)"
 set yrange [0.0001:10000]
@@ -37,11 +37,12 @@ set ylabel "micros/op"
 set logscale y
 
 plot \
-   '${OUTPUT}/fillseq-runc.dat' usin (\$0-0.5):(\$1/1000):(\$2/1000) w boxerrorbar fill patter 2 lc rgb "green" title "runc" ,\
-   '${OUTPUT}/fillseq-kata-runtime.dat' usin (\$0-0.3):(\$1/1000):(\$2/1000) w boxerrorbar fill patter 1 lc rgb "gray" title "kata-runtime" ,\
-   '${OUTPUT}/fillseq-runsc-ptrace.dat' usin (\$0-0.1):(\$1/1000):(\$2/1000) w boxerrorbar fill patter 3 lc rgb "blue" title "runsc(ptrace)" ,\
-   '${OUTPUT}/fillseq-native.dat' usin (\$0+0.1):(\$1/1000):(\$2/1000) w boxerrorbar fill patter 0 lc rgb "red" title "native" ,\
-   '${OUTPUT}/fillseq-lkl.dat' usin (\$0+0.3):(\$1/1000):(\$2/1000) w boxerrorbar fill patter 2 lc rgb "cyan" title "lkl"
+   '${OUTPUT}/fillseq-runc.dat' usin (\$0-0.3):(\$1):(\$2) w boxerrorbar fill patter 2 lt 1 lc rgb "green" title "runc" ,\
+   '${OUTPUT}/fillseq-kata-runtime.dat' usin (\$0-0.15):(\$1):(\$2) w boxerrorbar fill patter 1 lt 1 lc rgb "gray" title "runv" ,\
+   '${OUTPUT}/fillseq-runsc-ptrace.dat' usin (\$0-0.0):(\$1):(\$2) w boxerrorbar fill patter 3 lt 1 lc rgb "blue" title "runsc" ,\
+   '${OUTPUT}/fillseq-lkl.dat' usin (\$0+0.15):(\$1):(\$2) w boxerrorbar fill patter 4 lt 1 lc rgb "cyan" title "runu" ,\
+   '${OUTPUT}/fillseq-native.dat' usin (\$0+0.3):(\$1):(\$2) w boxerrorbar fill patter 0 lt 1 lc rgb "red" title "native"
+
 
 set terminal png lw 3 14 crop
 set output "${OUTPUT}/out/fillseq.png"
@@ -49,7 +50,7 @@ replot
 
 set terminal postscript eps lw 3 "Helvetica" 24
 set output "${OUTPUT}/out/fillrandom.eps"
-set xrange [-1.0:7.0]
+set xrange [-0.5:6.5]
 set xtics ('128' 0, '256' 1, '512' 2, '1024' 3, '1500' 4, '4096' 5, '8192' 6)
 set xlabel "Value size (bytes)"
 set yrange [0.0001:10000]
@@ -58,11 +59,12 @@ set ylabel "micros/op"
 set logscale y
 
 plot \
-   '${OUTPUT}/fillrandom-runc.dat' usin (\$0-0.5):(\$1/1000):(\$2/1000) w boxerrorbar fill patter 2 lc rgb "green" title "runc" ,\
-   '${OUTPUT}/fillrandom-kata-runtime.dat' usin (\$0-0.3):(\$1/1000):(\$2/1000) w boxerrorbar fill patter 1 lc rgb "gray" title "kata-runtime" ,\
-   '${OUTPUT}/fillrandom-runsc-ptrace.dat' usin (\$0-0.1):(\$1/1000):(\$2/1000) w boxerrorbar fill patter 3 lc rgb "blue" title "runsc(ptrace)" ,\
-   '${OUTPUT}/fillrandom-native.dat' usin (\$0+0.1):(\$1/1000):(\$2/1000) w boxerrorbar fill patter 0 lc rgb "red" title "native" ,\
-   '${OUTPUT}/fillrandom-lkl.dat' usin (\$0+0.3):(\$1/1000):(\$2/1000) w boxerrorbar fill patter 2 lc rgb "cyan" title "lkl"
+   '${OUTPUT}/fillrandom-runc.dat' usin (\$0-0.3):(\$1):(\$2) w boxerrorbar fill patter 2 lt 1 lc rgb "green" title "runc" ,\
+   '${OUTPUT}/fillrandom-kata-runtime.dat' usin (\$0-0.15):(\$1):(\$2) w boxerrorbar fill patter 1 lt 1 lc rgb "gray" title "runv" ,\
+   '${OUTPUT}/fillrandom-runsc-ptrace.dat' usin (\$0-0.0):(\$1):(\$2) w boxerrorbar fill patter 3 lt 1 lc rgb "blue" title "runsc" ,\
+   '${OUTPUT}/fillrandom-lkl.dat' usin (\$0+0.15):(\$1):(\$2) w boxerrorbar fill patter 4 lt 1 lc rgb "cyan" title "runu" ,\
+   '${OUTPUT}/fillrandom-native.dat' usin (\$0+0.3):(\$1):(\$2) w boxerrorbar fill patter 0 lt 1 lc rgb "red" title "native"
+
 
 set terminal png lw 3 14 crop
 set output "${OUTPUT}/out/fillrandom.png"
@@ -70,7 +72,7 @@ replot
 
 set terminal postscript eps lw 3 "Helvetica" 24
 set output "${OUTPUT}/out/readseq.eps"
-set xrange [-1.0:7.0]
+set xrange [-0.5:6.5]
 set xtics ('128' 0, '256' 1, '512' 2, '1024' 3, '1500' 4, '4096' 5, '8192' 6)
 set xlabel "Value size (bytes)"
 set yrange [0.0001:1]
@@ -79,11 +81,12 @@ set ylabel "micros/op"
 set logscale y
 
 plot \
-   '${OUTPUT}/readseq-runc.dat' usin (\$0-0.5):(\$1/1000):(\$2/1000) w boxerrorbar fill patter 2 lc rgb "green" title "runc" ,\
-   '${OUTPUT}/readseq-kata-runtime.dat' usin (\$0-0.3):(\$1/1000):(\$2/1000) w boxerrorbar fill patter 1 lc rgb "gray" title "kata-runtime" ,\
-   '${OUTPUT}/readseq-runsc-ptrace.dat' usin (\$0-0.1):(\$1/1000):(\$2/1000) w boxerrorbar fill patter 3 lc rgb "blue" title "runsc(ptrace)" ,\
-   '${OUTPUT}/readseq-native.dat' usin (\$0+0.1):(\$1/1000):(\$2/1000) w boxerrorbar fill patter 0 lc rgb "red" title "native" ,\
-   '${OUTPUT}/readseq-lkl.dat' usin (\$0+0.3):(\$1/1000):(\$2/1000) w boxerrorbar fill patter 2 lc rgb "cyan" title "lkl"
+   '${OUTPUT}/readseq-runc.dat' usin (\$0-0.3):(\$1):(\$2) w boxerrorbar fill patter 2 lt 1 lc rgb "green" title "runc" ,\
+   '${OUTPUT}/readseq-kata-runtime.dat' usin (\$0-0.15):(\$1):(\$2) w boxerrorbar fill patter 1 lt 1 lc rgb "gray" title "runv" ,\
+   '${OUTPUT}/readseq-runsc-ptrace.dat' usin (\$0-0.0):(\$1):(\$2) w boxerrorbar fill patter 3 lt 1 lc rgb "blue" title "runsc" ,\
+   '${OUTPUT}/readseq-lkl.dat' usin (\$0+0.15):(\$1):(\$2) w boxerrorbar fill patter 4 lt 1 lc rgb "cyan" title "runu" ,\
+   '${OUTPUT}/readseq-native.dat' usin (\$0+0.3):(\$1):(\$2) w boxerrorbar fill patter 0 lt 1 lc rgb "red" title "native"
+
 
 set terminal png lw 3 14 crop
 set output "${OUTPUT}/out/readseq.png"
@@ -91,7 +94,7 @@ replot
 
 set terminal postscript eps lw 3 "Helvetica" 24
 set output "${OUTPUT}/out/readrandom.eps"
-set xrange [-1.0:7.0]
+set xrange [-0.5:6.5]
 set xtics ('128' 0, '256' 1, '512' 2, '1024' 3, '1500' 4, '4096' 5, '8192' 6)
 set xlabel "Value size (bytes)"
 set yrange [0.0001:1]
@@ -100,11 +103,12 @@ set ylabel "micros/op"
 set logscale y
 
 plot \
-   '${OUTPUT}/readrandom-runc.dat' usin (\$0-0.5):(\$1/1000):(\$2/1000) w boxerrorbar fill patter 2 lc rgb "green" title "runc" ,\
-   '${OUTPUT}/readrandom-kata-runtime.dat' usin (\$0-0.3):(\$1/1000):(\$2/1000) w boxerrorbar fill patter 1 lc rgb "gray" title "kata-runtime" ,\
-   '${OUTPUT}/readrandom-runsc-ptrace.dat' usin (\$0-0.1):(\$1/1000):(\$2/1000) w boxerrorbar fill patter 3 lc rgb "blue" title "runsc(ptrace)" ,\
-   '${OUTPUT}/readrandom-native.dat' usin (\$0+0.1):(\$1/1000):(\$2/1000) w boxerrorbar fill patter 0 lc rgb "red" title "native" ,\
-   '${OUTPUT}/readrandom-lkl.dat' usin (\$0+0.3):(\$1/1000):(\$2/1000) w boxerrorbar fill patter 2 lc rgb "cyan" title "lkl"
+   '${OUTPUT}/readrandom-runc.dat' usin (\$0-0.3):(\$1):(\$2) w boxerrorbar fill patter 2 lt 1 lc rgb "green" title "runc" ,\
+   '${OUTPUT}/readrandom-kata-runtime.dat' usin (\$0-0.15):(\$1):(\$2) w boxerrorbar fill patter 1 lt 1 lc rgb "gray" title "runv" ,\
+   '${OUTPUT}/readrandom-runsc-ptrace.dat' usin (\$0-0.0):(\$1):(\$2) w boxerrorbar fill patter 3 lt 1 lc rgb "blue" title "runsc" ,\
+   '${OUTPUT}/readrandom-lkl.dat' usin (\$0+0.15):(\$1):(\$2) w boxerrorbar fill patter 4 lt 1 lc rgb "cyan" title "runu" ,\
+   '${OUTPUT}/readrandom-native.dat' usin (\$0+0.3):(\$1):(\$2) w boxerrorbar fill patter 0 lt 1 lc rgb "red" title "native"
+
 
 set terminal png lw 3 14 crop
 set output "${OUTPUT}/out/readrandom.png"
