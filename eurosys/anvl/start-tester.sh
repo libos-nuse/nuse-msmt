0df1e169055e18cad9d023fdf5dc92f9fd5331af
Hajime Tazaki <thehajime@gmail.com>
2020-05-14 15:12:35 +0900

    update startup file

--- eurosys/anvl/start-tester.sh
+++ eurosys/anvl/start-tester.sh
@@ -3,7 +3,7 @@
 # host network version
 sudo ifconfig eth0 hw ether 8c:85:90:55:a5:ee 
 docker run -it --detach-keys="ctrl-q,ctrl-q" --rm -v /home/tazaki:/home/tazaki \
-       	--net=host  --privileged=True --user=tazaki --hostname=ixanvl --name=ixanvl ixanvl:centos6.6 zsh
+       	--net=host  --privileged=True -e USER=tazaki --user=tazaki --hostname=ixanvl --name=ixanvl ixanvl:centos6.6 zsh
 
 # BAK: mac-vlan version
 #docker create -it --rm -v /home/tazaki:/home/tazaki  --mac-address="8c:85:90:55:a5:ee" \
